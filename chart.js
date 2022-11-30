a80a7887ae8619a512dc7a8b9e9b36212784a780
Munkhzaya.Gan <munkhzaya02111@gmail.com>
2022-11-30 21:32:50 +0800

    chart

--- chart.js
+++ chart.js
@@ -1,32 +1,9 @@
-var oilCanvas = document.getElementById("oilChart");
+// Chart.defaults.global.defaultFontFamily = "Lato";
+// Chart.defaults.global.defaultFontSize = 18;
 
-
-Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 18;
-
-var oilData = {
-    labels: [
-        "Saudi Arabia",
-        "Russia",
-        "Iraq",
-        "United Arab Emirates",
-        "Canada"
-    ],
-    datasets: [
-        {
-            data: [133.3, 86.2, 52.2, 51.2, 50.2],
-            backgroundColor: [
-                "#FF6384",
-                "#63FF84",
-                "#84FF63",
-                "#8463FF",
-                "#6384FF"
-            ]
-        }]
-};
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-  type: "pie",
+  type: "doughnut",
   data: {
     labels: ["OK", "WARNING", "CRITICAL", "UNKNOWN"],
     datasets: [
@@ -56,6 +33,6 @@ var myChart = new Chart(ctx, {
 });
 
 var pieChart = new Chart(oilCanvas, {
-  type: 'doughnut',
+  type: "doughnut",
   data: oilData
 });
