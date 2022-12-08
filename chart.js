5ca0781c535f549c3dc574e9fb3e7921638d7e10
Suvdaa1113 <suusar.suvd@gmail.com>
2022-12-08 22:50:35 +0800

    add some changes

--- chart.js
+++ chart.js
@@ -1,6 +1,3 @@
-// Chart.defaults.global.defaultFontFamily = "Lato";
-// Chart.defaults.global.defaultFontSize = 18;
-
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: "doughnut",
@@ -15,25 +12,14 @@ var myChart = new Chart(ctx, {
           "#7F3DFF",
           "#FCAC12",
           "#0077FF",
-          "#00A86B"
+          "#00A86B",
         ],
-        borderColor: [
-          "rgba(255,99,132,1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)"
-        ],
-        borderWidth: 0.5
-      }
-    ]
+        borderWidth: 0.5,
+      },
+    ],
   },
   options: {
     //cutoutPercentage: 40,
-    responsive: false
-  }
-});
-
-var pieChart = new Chart(oilCanvas, {
-  type: "doughnut",
-  data: oilData
+    responsive: false,
+  },
 });
